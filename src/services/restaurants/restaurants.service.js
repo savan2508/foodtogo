8ba4b250cbac1a38b908794a32dc72a19a1417cd
savan2508 <sawanpatel2508@gmail.com>
2023-12-28 14:20:22 -0500

    Add documentation

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,6 +1,19 @@
+/**
+ * @module services/restaurants/restaurants.service
+ * @description Service functions for handling restaurant data.
+ * @exports restaurantsRequest
+ * @exports restaurantsTransform
+ */
+
 import { mockImages, mocks } from "./mock";
 import camelize from "camelize";
 
+/**
+ * @function restaurantsRequest
+ * @description Fetches restaurant data based on the provided location.
+ * @param {string} location - Location coordinates in the format "latitude,longitude".
+ * @returns {Promise} A Promise that resolves with the restaurant data or rejects with an error message.
+ */
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
@@ -11,16 +24,26 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
+/**
+ * @function restaurantsTransform
+ * @description Transforms raw restaurant data into a standardized format.
+ * @param {Object} data - Raw restaurant data.
+ * @param {Array} data.results - Array of restaurant results.
+ * @returns {Array} An array of transformed restaurant data with added properties.
+ */
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map((restaurant) => {
+    // Map photos to random mock images
     restaurant.photos = restaurant.photos.map((p) => {
       return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
     });
+    // Add additional properties for convenience
     return {
       ...restaurant,
       isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
     };
   });
+  // Convert keys to camel case for consistency
   return camelize(mappedResults);
 };
