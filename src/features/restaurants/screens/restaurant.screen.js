d84f19e7bfd0e7a999ff50b0e3cb258d4b2d4ba3
savan2508 <sawanpatel2508@gmail.com>
2023-12-28 14:06:53 -0500

    Add documentation

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,36 +1,61 @@
+/**
+ * @module restaurant.screen
+ * @description Screen component for displaying a list of restaurants with a search bar and loading indicator.
+ * @exports RestaurantsScreen
+ */
+
 // Import necessary modules from React Native and third-party libraries
+import React, { useContext } from "react";
 import { FlatList, View } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { styled } from "styled-components/native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
-import { useContext } from "react";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { ActivityIndicator, MD2Colors } from "react-native-paper";
 
 // Styled components for the screen layout
+/**
+ * @component
+ * @description Styled View component for the search container.
+ */
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+/**
+ * @component
+ * @description Styled FlatList component for displaying the list of restaurants.
+ */
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
     padding: 16,
   },
 })``;
 
+/**
+ * @component
+ * @description Styled ActivityIndicator component for displaying a loading indicator.
+ */
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
 `;
 
+/**
+ * @component
+ * @description Styled View component for a container to position the loading indicator.
+ */
 const LoadingContainer = styled(View)`
   position: absolute;
   top: 50%;
   left: 50%;
 `;
 
-// Main functional component for the RestaurantsScreen
+/**
+ * @component
+ * @description Main functional component for the RestaurantsScreen.
+ */
 export const RestaurantsScreen = () => {
   // Get restaurant data from the context
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
